eaa5cd2613586b3957c15750d412ddb84fcd3b3f
HopeMashal <mashal.hope@gmail.com>
2021-11-24 09:16:17 +0200

    edit

--- Ex.stack.tree.js
+++ Ex.stack.tree.js
@@ -4,3 +4,28 @@ addToEnd(x)
 isEmpty()
 isFull()
 removeFirst(x) - removes first element that equals x */
+
+class TreeNode {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+const traverseTree = (node) => {
+    if (!node) {
+        return;
+    }
+    traverseTree(node.left);
+    traverseTree(node.right);
+    console.log(node.data);
+}
+let head = new TreeNode(20);
+let node1 = new TreeNode(15);
+let node2 = new TreeNode(1337);
+let node3 = new TreeNode(2000);
+head.left = node1;
+head.right = node2;
+node2.right = node3;
+traverseTree(head);
+// console.log(head)
